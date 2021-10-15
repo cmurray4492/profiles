e4bcedb711f2a75918c6b3f9de1e0c6dbf63ecff
Craig Murray <cmurray4492@gmail.com>
2021-10-14 21:21:41 -0500

    User Profile 2.1

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 # Create your views here.
 def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -13,27 +18,22 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print('Username does not exist')
-
-        # print(username)
-        # print(password)
+            messages.error(request, 'Username does not exist')
 
         user = authenticate(request, username=username, password=password)
-
-        # print(username)
-        # print(password)
 
         if user is not None:
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username or Password incorrect')
+            messages.error(request,'Username or Password incorrect')
 
     return render(request, 'users/login_register.html')
 
 
 def logoutUser(request):
     logout(request)
+    messages.error(request, 'User was logged out!')
     return redirect('profiles')
 
 
