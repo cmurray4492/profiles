296ca483984189820d1ab8ada53e22606824846c
Craig Murray <cmurray4492@gmail.com>
2022-01-04 20:33:33 -0600

    Final Profile CRUD

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -65,4 +65,4 @@ def deleteProject(request, pk):
         return redirect('projects')
 
     context = {'object': project}
-    return render(request, 'projects/delete_template.html', context)
+    return render(request, 'delete_template.html', context)
